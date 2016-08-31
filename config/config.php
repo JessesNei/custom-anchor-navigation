042e76b99d2c17d7425323e1f4fcdfdf9978cbb7
Katrin Beutler <katrinbeutler@Katrins-MacBook-Pro.local>
2016-08-31 13:05:03 +0200

    no message

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+* @file config.php
+* @author Katrin Beutler
+* @version 3.5.6
+* @package kb.contao.extensions.ce
+* @copyright Katrin Beutler
+*/
+
+
+
+//$GLOBALS['TL_CTE']['slider']['slider_group'] = 'SliderGroup';
+
+
+?>
