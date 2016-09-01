a0c3d614b5a1b6641ee5257803d52693cdfc31e0
Katrin Beutler <katrinbeutler@Katrins-MacBook-Pro.local>
2016-09-01 12:00:05 +0200

    add some files to module add composer son file

---
+++ languages/en/tl_article.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file tl_article.php
+ * @author Katrin Beutler
+ * @version 3.5.6
+ * @package kb.contao.extensions.ce
+ * @copyright Katrin Beutler
+ */
+
+
+$GLOBALS['TL_LANG']['tl_article']['showAnchor'] = array('Show as Anchor', 'Decides if this article should be showed as anchor.');
+$GLOBALS['TL_LANG']['tl_article']['titleAnchor'] = array('Indivisual Anchortext', 'Can contain HTML (f. e. span tag from iconfont). Fallback is Articletitle.');
